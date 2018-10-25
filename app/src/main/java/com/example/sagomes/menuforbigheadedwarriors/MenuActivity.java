a1270c346787ef7d9e8bd37eff741959b375d635
SaGomes <showzen97@gmail.com>
2018-10-25 14:22:20 +0100

    trading

--- app/src/main/java/com/example/sagomes/menuforbigheadedwarriors/MenuActivity.java
+++ app/src/main/java/com/example/sagomes/menuforbigheadedwarriors/MenuActivity.java
@@ -1,13 +1,27 @@
 package com.example.sagomes.menuforbigheadedwarriors;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MenuActivity extends AppCompatActivity {
 
+    private Button tradingButton;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu);
+        tradingButton = (Button) findViewById(R.id.tradingButton);
+        final Intent intent = new Intent(this,TradingActivity.class);
+        tradingButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(intent);
+            }
+        });
     }
+
+
 }
